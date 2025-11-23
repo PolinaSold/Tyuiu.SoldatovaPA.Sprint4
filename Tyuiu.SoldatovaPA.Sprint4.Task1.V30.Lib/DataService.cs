d8bc05cb5a9773e31fe8025d31b18e8837d9d647
PolinaSold <clevnay@mail.ru>
2025-11-23 22:15:42 +0500

    Task1

---
+++ Tyuiu.SoldatovaPA.Sprint4.Task1.V30.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint4;
+using Tyuiu.SoldatovaPA.Sprint4.Task1.V30.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint4.Task1.V30.Lib
+{
+    public class DataService : ISprint4Task1V30
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 0;
+
+            foreach (int num in array)
+            {
+                if (num % 2 == 0) // Чётные числа
+                {
+                    sum += num;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.SoldatovaPA.Sprint4.Task1.V30/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Tyuiu.SoldatovaPA.Sprint4.Task1.V30.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint4.Task1.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнила: Солдатова П. А. | ИСПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                              *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                      *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #30                                                            *");
+            Console.WriteLine("* Выполнила: Солдатова П. А. | ИСПБ-25-1                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан одномерный целочисленный массив. Вычислить сумму чётных элементов *");
+            Console.WriteLine("* массива.                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = { 6, 7, 8, 7, 6, 5, 6, 9, 9, 5, 7, 9, 7, 8, 7 };
+
+            Console.Write("Массив: { ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < array.Length - 1) Console.Write(", ");
+            }
+            Console.WriteLine(" }");
+
+            DataService ds = new DataService();
+            int result = ds.Calculate(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Сумма четных элементов = {result}");
+            Console.ReadKey();
+        }
+    }
+}
