09783899de430ea3a418a0386bdf109e90c2f4e8
PolinaSold <clevnay@mail.ru>
2025-11-23 22:47:31 +0500

    Task2

--- Tyuiu.SoldatovaPA.Sprint4.Task2.V23.Lib/DataService.cs
+++ Tyuiu.SoldatovaPA.Sprint4.Task2.V23.Lib/DataService.cs
@@ -10,7 +10,8 @@ namespace Tyutu.SoldatovaPA.Sprint4.Task2.V23.Lib
 
             foreach (int num in array)
             {
-                if (num % 2 != 0) // проверка на нечетность
+                // Сумма ЧЕТНЫХ элементов
+                if (num % 2 == 0)
                 {
                     sum += num;
                 }
--- Tyuiu.SoldatovaPA.Sprint4.Task2.V23.Test/DataServiceTest.cs
+++ Tyuiu.SoldatovaPA.Sprint4.Task2.V23.Test/DataServiceTest.cs
@@ -11,28 +11,12 @@ namespace Tyutu.SoldatovaPA.Sprint4.Task2.V23.Test
         {
             DataService ds = new DataService();
 
-            // Используем тот же массив, что и в тестовой системе
             int[] array = { 5, 5, 4, 3, 3, 3, 3, 7, 4, 6, 3, 6, 3, 6, 5 };
 
             int result = ds.Calculate(array);
-            // Нечетные: 5,5,3,3,3,3,7,3,3,5 = 40
-            // Но ожидается 26 - значит в задании ДРУГОЙ массив!
+            // ЧЕТНЫЕ элементы: 4, 4, 6, 6, 6
+            // Сумма: 4 + 4 + 6 + 6 + 6 = 26
 
-            Assert.AreEqual(26, result);
-        }
-
-        [TestMethod]
-        public void TestWithCorrectArray()
-        {
-            DataService ds = new DataService();
-
-            // Найдем массив, который дает сумму 26
-            // 26 можно получить как: 3+3+3+3+3+3+3+5 = 26 (7 троек и одна пятерка)
-            // Или: 3+3+3+3+3+5+6 - но 6 четная
-            // Или: 5+5+5+5+3+3 = 26
-            int[] array = { 5, 5, 5, 5, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4 };
-
-            int result = ds.Calculate(array);
             Assert.AreEqual(26, result);
         }
     }
